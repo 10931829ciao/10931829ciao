13fc5babcb8ae4faaf7cd99e759f7be2caab40da
喬 <84886055+10931829ciao@users.noreply.github.com>
2021-05-27 12:15:00 +0800

    Add files via upload

---
+++ work2.java
@@ -0,0 +1,12 @@
+public class work2
+{
+    public static void main(String args[])
+    {
+        String a = "Hello ";
+        String b = "World*";
+        System.out.println(a+b);
+        int i = 1;
+        int j = 2;
+        System.out.println(i+j);
+    }
+}
